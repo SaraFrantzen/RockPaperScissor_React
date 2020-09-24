40d244586941d5bd6eb9aba298867ad50521ef1d
sara <saralundkvist86@gmail.com>
2020-09-24 23:51:50 +0200

    adding axios to be able to make a call to the rps-api, configured index.js to use the deployed haroku application in productionmode and localhost in development. Creating integrationtest userCanLogin which should return a sucessmessage for happy path and errormessage for sad path. Creating new component LoginForm which is rendered in App. All test goes green

---
+++ cypress/integration/userCanLogin.feature.js
@@ -0,0 +1,27 @@
+describe("User authenticates", () => {
+    beforeEach(() => {
+      cy.visit("/");
+    });
+  
+    it("successfully with valid credentials", () => {
+      cy.get("#login").click();
+      cy.get("#login-form").within(() => {
+        cy.get("#email").type("user@mail.com");
+        cy.get("#password").type("password");
+        cy.get('button').contains('Submit').click()
+
+     
+      });
+      cy.get("#message").should("contain", "You're logged in as: user@mail.com");
+    });
+  
+    it("unsuccessfully with invalid credentials", () => {
+      cy.get("#login").click();
+      cy.get("#login-form").within(() => {
+        cy.get("#email").type("user@mail.com");
+        cy.get("#password").type("wrongpassword");
+        cy.get('button').contains('Submit').click()
+      });
+      cy.get("#message").should("contain", "Invalid login credentials. Please try again.");
+    });
+  });
